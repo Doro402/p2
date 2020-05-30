0c30047f5b512917a818fc097400252a7354517b
Doro402 <63165530+Doro402@users.noreply.github.com>
2020-05-30 14:58:27 +0200

    Initial commit

---
+++ pages/connexion.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="container-header">
+        <div class="title">Login</div>
+    </div>
+    <div class="container-body">
+        <form action="" method="post">
+<div class="input-form">
+    <div class="icon-form"></div>
+    <input type="text" class="form-control" name="" id="">
+    <div class="error-form"></div>
+</div>
+
+
+        </form>
+    </div>
+</div>
